12a4815c093ed22bd4946cee20e1fc06cebca4c1
hasuto <y21511@iwate-it.ac.jp>
2022-07-08 16:04:33 +0900

    kousin

---
+++ apurijisyu7/template.js
@@ -0,0 +1,14 @@
+
+
+// グローバルで使用する変数の宣言などをここに追加
+
+window.onload = appInit;
+
+// function appInit(){
+//     displayPanel = document.getElementById("displayPanel");
+//     displayPanel.textContent = "テンプレート動作テスト";
+// }
+
+var cards, card1, card2;
+var places, matched;
+var clickEvent;
