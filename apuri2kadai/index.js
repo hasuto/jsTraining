b01cf2553b831b2939b38044b3740ad4029af97d
hasuto <y21511@iwate-it.ac.jp>
2022-09-02 16:13:12 +0900

    kousin

--- apuri2kadai/index.js
+++ apuri2kadai/index.js
@@ -1,12 +1,31 @@
 // 文字列
-var sentence = document.querySelector('input').innerText;
+var sentence = document.getElementById("sentence");
+var button = document.getElementById("selectbutton");
 
 // 正規表現オブジェクトをリテラルで定義
-var pattern = /光/;
+var pattern = [/死ね/, /くそ/, /あほ/, /ばか/];
+var bancolect = [];
+var batCount = 0;
 
 // 文字列を正規表現で判定
-if( pattern.test(sentence) ) {
-  document.querySelector('#result b').innerText = "「光」の単語は含まれています";
-}else{
-  document.querySelector('#result b').innerText = "「光」の単語は含まれていません";
+for(var i = 0; i < pattern.length; i++){
+  if( pattern[i].test(localStorage.getItem("text")) ) {
+    document.querySelector('#inputtext b').innerText = localStorage.getItem("text");
+    var word = "'" +  pattern[i] + "'";
+    bancolect.push(word.substr(2, word.length - 4));
+    document.querySelector('#banword b').innerText = bancolect;
+    batCount++;
+  }
+  if(batCount != 0){
+    document.querySelector('#result b').innerText = "ネットの悪口、もうやめましょうよ!!!... 時間がも゛ったいだいっ!!!";
+  }else{
+    document.querySelector('#result b').innerText = "善良な文章です。";
+  }
 }
+
+
+button.addEventListener("click", function(){
+  localStorage.removeItem("text");
+  localStorage.setItem("text", sentence.value);
+  window.location.reload();
+}, false);
